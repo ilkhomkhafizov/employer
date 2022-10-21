7d49fcc63e77cfb5b2af7e96f3cc38c67f7124a8
Ilkhom Khafizov <ilhomhafizov@MacBook-Pro-Ilhom.local>
2022-10-21 13:16:28 +0500

    creted employee models and set in admin panel

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+from employee.models import Department, Employee
+
+admin.site.register(
+    Department,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title'
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+
+admin.site.register(Employee)
--- employee/models.py
+++ employee/models.py
@@ -1,2 +1,30 @@
 from django.db import models
-# Create your models here.
+from django.utils import timezone
+from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Department(MPTTModel):
+    name = models.CharField(max_length=150, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('department', kwargs={'pk': self.pk})
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    employment_position = models.CharField(max_length=200)
+    employment_start_date = models.DateField(auto_now_add=False)
+    salary = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    department = TreeForeignKey(Department, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
