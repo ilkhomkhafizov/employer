b67123cfae294b3d7aecfeabcb5bd0b13b08d18d
Ilkhom Khafizov <ilhomhafizov@MacBook-Pro-Ilhom.local>
2022-10-21 12:26:42 +0500

    connect with database and written some configs

---
+++ core/config.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+from decouple import config, Csv
+
+
+class Config:
+
+    def __init__(self):
+        self.logger = logging.getLogger(__class__.__name__)
+        # Django
+        self.django_secret_key = config('django_secret_key', cast=str)
+        self.debug = config('debug', bool)
+        self.allowed_hosts = config('allowed_hosts', cast=Csv())
+        self.cors_origins = config('cors_origins', cast=Csv())
+        # DB
+        self.db_host = config('db_host', cast=str)
+        self.db_name = config('db_name', cast=str)
+        self.db_user = config('db_user', cast=str)
+        self.db_password = config('db_password', cast=str)
+        self.db_port = config('db_port', cast=int)
+
+
+logger = logging.getLogger('config')
+
+try:
+    CONFIG = Config()
+except Exception as error:
+    logger.critical(f'config is broken {error}')
+    sys.exit(1)
